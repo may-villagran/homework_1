193f7a831501c5b05d228ff0317f02c9b4af8199
may-villagran <maylenvillagran2004@gmail.com>
2025-03-14 20:17:00 +0000

    Creación archivo gitignore, creacion del archivo ejercicio4.h

---
+++ ejercicio4/ejercicio4.h
@@ -0,0 +1,16 @@
+#ifndef EJERCICIO4_H
+#define EJERCICIO4_H
+
+#include <chrono>
+#include <iostream>
+using namespace std;
+
+constexpr bool coincidencia_texto_compilacion(const char* str1, const char* str2) {
+    if (*str1 == '\0' && *str2 == '\0') return true;
+    else if (*str1 != *str2) return false;
+    return coincidencia_texto_compilacion(++str1, ++str2);
+}
+
+bool coincidencia_texto(const char* str1, const char* str2);
+
+#endif 
